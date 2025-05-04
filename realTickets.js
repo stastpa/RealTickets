613d28dd3c90d4a536cb0321c041ef8ee08c9903
stastpa <stastny.patrik18@seznam.cz>
2025-05-04 13:32:33 +0200

    Update realTickets.js

--- realTickets.js
+++ realTickets.js
@@ -11,10 +11,7 @@ const client = new Client({
 });
 
 async function checkTickets() {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  });
+  const browser = await puppeteer.launch({headless: false, args: ['--disable-features=site-per-process']})
   const page = await browser.newPage();
 
   try {
